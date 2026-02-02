0da2f25163232873c09775473aa54894d84bcace
Dhananjay <dhananjay.pandey72@gmail.com>
2026-02-02 23:55:05 +0530

    User login and signup with routes

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    phoneNo: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^[0-9]{10}$/, "Invalid phone number"],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
